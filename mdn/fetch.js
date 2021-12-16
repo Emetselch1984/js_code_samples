272b9fc17646ef3b45575d131f6e43ec61ee571e
RyomaKaneko <banzai.nippon2030@gmail.com>
2021-12-16 09:30:24 +0900

    feat: Fetchの使い方

--- mdn/fetch.js
+++ mdn/fetch.js
@@ -56,3 +56,154 @@ fetch('https://example.com/profile', {
     .catch((error) => {
         console.error('Error:', error);
     });
+// ファイルのアップロード
+const formData = new FormData();
+const fileField = document.querySelector('input[type="file"]');
+
+formData.append('username', 'abc123');
+formData.append('avatar', fileField.files[0]);
+
+fetch('https://example.com/profile/avatar', {
+    method: 'PUT',
+    body: formData
+})
+    .then(response => response.json())
+    .then(result => {
+        console.log('Success:', result);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+// 複数ファイルのアップロード
+
+const formData = new FormData();
+const photos = document.querySelector('input[type="file"][multiple]');
+
+formData.append('title', 'My Vegas Vacation');
+for (let i = 0; i < photos.files.length; i++) {
+    formData.append('photos', photos.files[i]);
+}
+
+fetch('https://example.com/posts', {
+    method: 'POST',
+    body: formData,
+})
+    .then(response => response.json())
+    .then(result => {
+        console.log('Success:', result);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+// テキストファイルの1行ずつの処理
+
+async function* makeTextFileLineIterator(fileURL) {
+    const utf8Decoder = new TextDecoder('utf-8');
+    const response = await fetch(fileURL);
+    const reader = response.body.getReader();
+    let { value: chunk, done: readerDone } = await reader.read();
+    chunk = chunk ? utf8Decoder.decode(chunk) : '';
+
+    const re = /\n|\r|\r\n/gm;
+    let startIndex = 0;
+    let result;
+
+    for (;;) {
+        let result = re.exec(chunk);
+        if (!result) {
+            if (readerDone) {
+                break;
+            }
+            let remainder = chunk.substr(startIndex);
+            ({ value: chunk, done: readerDone } = await reader.read());
+            chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
+            startIndex = re.lastIndex = 0;
+            continue;
+        }
+        yield chunk.substring(startIndex, result.index);
+        startIndex = re.lastIndex;
+    }
+    if (startIndex < chunk.length) {
+        // last line didn't end in a newline char
+        yield chunk.substr(startIndex);
+    }
+}
+
+async function run() {
+    for await (let line of makeTextFileLineIterator(urlOfFile)) {
+        processLine(line);
+    }
+}
+
+run();
+
+// fetchが成功したかチェックする
+fetch('flowers.jpg')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.blob();
+    })
+    .then(myBlob => {
+        myImage.src = URL.createObjectURL(myBlob);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+// Headers
+const content = 'Hello World';
+const myHeaders = new Headers();
+myHeaders.append('Content-Type', 'text/plain');
+myHeaders.append('Content-Length', content.length.toString());
+myHeaders.append('X-Custom-Header', 'ProcessThisImmediately');
+
+const myHeaders = new Headers({
+    'Content-Type': 'text/plain',
+    'Content-Length': content.length.toString(),
+    'X-Custom-Header': 'ProcessThisImmediately'
+});
+console.log(myHeaders.has('Content-Type')); // true
+console.log(myHeaders.has('Set-Cookie')); // false
+myHeaders.set('Content-Type', 'text/html');
+myHeaders.append('X-Custom-Header', 'AnotherValue');
+
+console.log(myHeaders.get('Content-Length')); // 11
+console.log(myHeaders.get('X-Custom-Header')); // ['ProcessThisImmediately', 'AnotherValue']
+
+myHeaders.delete('X-Custom-Header');
+console.log(myHeaders.get('X-Custom-Header')); // [ ]
+
+const myResponse = Response.error();
+try {
+    myResponse.headers.set('Origin', 'http://mybank.com');
+} catch (e) {
+    console.log("銀行のふりをしないで下さい！");
+}
+
+fetch(myRequest)
+    .then(response => {
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('application/json')) {
+            throw new TypeError("Oops, we haven't got JSON!");
+        }
+        return response.json();
+    })
+    .then(data => {
+        /* process your data further */
+    })
+    .catch(error => console.error(error));
+const myBody = new Blob();
+
+addEventListener('fetch', function(event) {
+    // ServiceWorker intercepting a fetch
+    event.respondWith(
+        new Response(myBody, {
+            headers: { 'Content-Type': 'text/plain' }
+        })
+    );
+});
+
